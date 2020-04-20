a276e5b400a9e1f25af5951f91521914e5943e0b
William Carter <wcarter312@gmail.com>
2020-04-20 11:38:19 -0500

    Update and rename homecoming.ts to thunderspy.ts

--- src/thunderspy.ts
+++ src/thunderspy.ts
@@ -2,19 +2,15 @@ import {IGameMapData, IServerData, IServerGroupData, ServerGroupStatus} from "co
 import {Maps} from "./map/_maps";
 import {IArchetypeData} from "coh-content-db/dist/types/archetype";
 
-export class Homecoming implements IServerGroupData {
-    public readonly key: string = "homecoming";
-    public readonly name: string = "Homecoming";
-    public readonly description: string = "City of Heroes: Homecoming - [Forums](https://forums.homecomingservers.com/)";
+export class Thunderspy implements IServerGroupData {
+    public readonly key: string = "thunderspy";
+    public readonly name: string = "Thunderspy";
+    public readonly description: string = "City of Heroes: Thunderspy - [Website](https://thunderspygaming.net/)";
     public readonly status: ServerGroupStatus[] = [];
     public readonly servers: IServerData[] = [
-        {name: "Everlasting"},
-        {name: "Excelsior"},
-        {name: "Indomitable"},
-        {name: "Reunion"},
-        {name: "Torchbearer"}
+        {name: "COXG"}
     ];
-    public readonly repository: string = "https://github.com/n15g/coh-content-db-homecoming";
+    public readonly repository: string = "https://github.com/n15g/coh-content-db-thunderspy";
 
     public readonly archetypes: IArchetypeData[] = [
         {key: "arachnos-soldier", name: "Arachnos Soldier"},
@@ -28,7 +24,6 @@ export class Homecoming implements IServerGroupData {
         {key: "mastermind", name: "Mastermind"},
         {key: "peacebringer", name: "Peacebringer"},
         {key: "scrapper", name: "Scrapper"},
-        {key: "sentinel", name: "Sentinel"},
         {key: "stalker", name: "Stalker"},
         {key: "tanker", name: "Tanker"},
         {key: "warshade", name: "Warshade"},
